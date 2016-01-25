7567a2c41bdd0f44c572f34664766a1d98e2525c
Anikait Singh <singh.anikait@gmail.com>
2016-01-24 23:53:48 -0500

    This is the final outcome of my endeavors of this project

---
+++ Coordinator.java
@@ -0,0 +1,67 @@
+package pathFinder;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Coordinator {
+	
+	public static void main(String[] args){		
+		// instantiation and initialization
+		String inputStr;
+		ArrayList<String> arrL = new ArrayList<String>();
+		char[][] arr = null;
+		String file = "maze.txt";
+
+		// input maze.txt as lines of strings
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			while((inputStr = reader.readLine())!=null){
+				arrL.add(inputStr);
+			}
+			reader.close();
+		}catch(FileNotFoundException ex){
+			ex.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//arraylist of strings to 2D array of chars
+		if(arrL != null){
+			arr = new char[arrL.size()][arrL.get(0).length()];
+			for(int i = 0; i< arrL.size(); i++){
+				inputStr = arrL.get(i); //reuse of string
+				for(int j = 0; j< arrL.get(0).length(); j++){
+					arr[i][j] = inputStr.charAt(j);
+				}
+			}
+		}
+		
+		print2DCharArray(arr);
+		//maze finding algorithm
+		printCoordinateArray(MazeFinder.findPath(arr));
+	}
+	
+	
+	//prints a 2 dimensional character array
+	public static void print2DCharArray(char[][] arr){
+		System.out.println("{ ");
+		for(int i = 0; i < arr.length; i++){
+			System.out.print("[ ");
+			for(int j = 0; j < arr[0].length; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println("]");
+		}
+		System.out.println("\n}\n");
+	}
+	
+	
+	//prints a single dimensional Coordinate array
+	public static void printCoordinateArray(ArrayList<Coordinate> arr){
+		Coordinate coord;
+		for(int i = 0; i < arr.size(); i++){
+			coord = arr.get(i);
+			System.out.println("(" + coord.getX() + "," + coord.getY() + ")");
+		}
+	}
+}
---
+++ MazeFinder.java
@@ -0,0 +1,139 @@
+package pathFinder;
+
+import java.util.ArrayList;
+
+public class MazeFinder {
+	
+	private static ArrayList<Coordinate> path = new ArrayList<Coordinate>(); //has the list of coordinates one must follow
+	//finds the beginning and ending point
+	public static Coordinate[] findStartAndFinish(char[][] arr, char start, char finish){
+		Coordinate Start = null, Finish = null, a[];
+		for(int i = 0; i< arr.length; i++){
+			for(int j = 0; j< arr[0].length; j++){
+				if(arr[i][j] == start){
+					Start = new Coordinate(i,j);
+				}
+				if(arr[i][j] == finish){
+					Finish = new Coordinate(i,j);
+				}
+			}
+		}
+		a = new Coordinate[2];
+		a[0] = Start;
+		a[1] = Finish;
+		return a;
+	}
+	
+	
+	//is called by coordinator
+	public static ArrayList<Coordinate> findPath(char[][] arr){
+		Coordinate[] beg = findStartAndFinish(arr, 'O', 'X');
+		return initFind(arr, beg[0].getX(), beg[0].getY(), beg[1].getX(), beg[1].getY());
+	}
+	
+	
+	//initializes the find and finishes it up
+	public static ArrayList<Coordinate> initFind(char[][] arr,int x1, int y1, int x2, int y2){
+		findASolution(arr, x1,y1,x2,y2);
+		return path;
+	}
+	
+	
+	//finds a possible path
+	public static boolean findASolution(char[][] arr, int x1, int y1, int x2, int y2){
+		if(arr[x1][y1] == '|'){
+			return false;
+		}
+		
+		if(arr[x1][y1] != 'O'){
+			arr[x1][y1] = '1';
+		}
+		
+		path.add(new Coordinate(x1,y1));
+		
+		if(x1+1 < arr.length){
+			if (arr[x1+1][y1] == 'P'){
+				if(findASolution(arr, x1 + 1, y1, x2, y2)){
+					return true;
+				}
+			}
+			if(arr[x1+1][y1] == 'X'){
+				path.add(new Coordinate(y1,x1+1));
+				return true;
+			}
+		}
+		if(y1+1 < arr[0].length){
+			if (arr[x1][y1+1] == 'P'){
+				if(findASolution(arr, x1, y1+1, x2, y2)){
+					return true;
+			}
+			}
+			if(arr[x1][y1+1] == 'X'){
+				path.add(new Coordinate(y1+1,x1));
+				return true;
+			}
+		}
+		if(x1-1 >= 0){
+			if (arr[x1-1][y1] == 'P'){
+				if(findASolution(arr, x1 - 1, y1, x2, y2)){
+					return true;
+				}
+			}
+			if(arr[x1-1][y1] == 'X'){
+				path.add(new Coordinate(y1, x1-1));
+				return true;
+			}
+		}
+		if(y1-1 >= 0){
+			if (arr[x1][y1-1] != 'O'){
+				if(findASolution(arr, x1, y1-1, x2, y2)){
+					return true;
+			}
+			}
+			if(arr[x1][y1-1] == 'X'){
+				path.add(new Coordinate(y1-1,x1));
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+
+/*
+ * internal class which stores an x and a y value and is used to pass that efficiently
+ */
+class Coordinate{
+	private int x, y;	
+	
+	public Coordinate(int _x, int _y){
+		x = _x;
+		y = _y;
+	}
+	
+	
+	public void setX(int _x){
+		x = _x;
+	}
+	
+	
+	public void setY(int _y){
+		y = _y;
+	}
+	
+	
+	public double getDistance(Coordinate c){
+		return (Math.sqrt( Math.pow((x - c.getX()),2) + Math.pow((y - c.getY()),2) ));
+	}
+	
+	
+	public int getX(){
+		return x;
+	}
+	
+	
+	public int getY(){
+		return y;
+	}
+}
+
